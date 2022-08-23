21d9348d19aa24654d65f697c824aeac4cf1ec3f
BlankRip <aniketh_g5@outlook.com>
2022-08-23 09:07:10 +0530

    Base world building

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -6,7 +6,19 @@ namespace BlockyWorld {
     public class World : MonoBehaviour
     {
         public static Vector3 worldDimensions = new Vector3(3, 3, 3);
-        public static Vector3 cunkDimensions = new Vector3(10, 10, 10);
+        public static Vector3Int chunkDimensions = new Vector3Int(10, 10, 10);
         public GameObject chunkPrefab;
+
+        private void Start() {
+            for (int z = 0; z < worldDimensions.z; z++) {
+                for (int y = 0; y < worldDimensions.y; y++) {
+                    for (int x = 0; x < worldDimensions.x; x++) {
+                        Chunk chunk = Instantiate(chunkPrefab).GetComponent<Chunk>();
+                        Vector3 position = new Vector3(x * chunkDimensions.x, y * chunkDimensions.y, z * chunkDimensions.z);
+                        chunk.CreateChunk(chunkDimensions, position);
+                    }
+                }
+            }
+        }
     }
 }
